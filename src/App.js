8f9f1054d721a5904193f7b299aa78812d918413
vishal dsouza <vishal.denzil@ezedox.com>
2020-05-23 10:48:15 +0530

    changes for the boat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ChatBot, { Loading } from 'react-simple-chatbot';
+import ChatBot, { Loading } from './react-simple-chatbot/lib/index';
 import {promise} from './server'
 
 class DBPedia extends Component {
@@ -19,12 +19,18 @@ class DBPedia extends Component {
     const { previousStep } = this.props;
     const value = previousStep.message;
 
-    promise("reply", value).then((res) => {
+    let extra = Math.floor(Math.random() * 3 + 1);
+
+    promise("reply", value, extra).then((res) => {
       this.setState({ 
         loading : false,
-        result: res
+        result: res.value,
+
       })
-      this.props.triggerNextStep(res ,'search');
+
+
+
+      this.props.triggerNextStep({nextStep :  res.next , next : true});
 
     })
     
@@ -51,62 +57,89 @@ DBPedia.defaultProps = {
   triggerNextStep: undefined,
 };
 
-const ExampleDBPedia = () => (
-  <ChatBot
-    steps={[
-      {
-        id: '1',
-        message: 'what is your name?',
-        trigger: 'user',
-      },
-      {
-      id: 'user',
-      user: true,
-      trigger: 'previous',
-     },
-     {
-        id: 'previous',
-        message: 'Hi {previousValue}, nice to meet you!',
-        trigger: 'areyou',
-      },
-      {
-        id: 'areyou',
-        message: 'Why are you here?',
-        trigger: 'search',
-      },
-      {
-        id: 'search',
-        user: true,
-        trigger: '3',
-      },
-      {
-        id: '3',
-        component: <DBPedia />,
-        waitAction: true,
-        trigger: 'selectsomething',
-      },
-      {
-        id: 'selectsomething',
-        message: 'Select some of the things',
-        trigger: '4',
-      },
-      {
-          id: '4',
-          options: [
-            { value: 1, label: 'Enjoy', trigger: '5' },
-            { value: 2, label: 'News', trigger: '5' },
-            { value: 3, label: 'Chatting', trigger: '5' },
-          ]
-        },
-      {
-        id: '5',
-        component: <DBPedia />,
-        waitAction: true,
-        trigger: 'search',
-      }
-      
-    ]}
-  />
-);
+class ExampleDBPedia extends Component {
+  state = {
+    steps: []
+  }
 
+  componentDidMount() {
+    let steps = [
+        {
+          id: '1',
+          message: 'what is your name?',
+          trigger: 'user',
+        },
+        {
+        id: 'user',
+        user: true,
+        trigger: 'previous',
+      },
+      {
+          id: 'previous',
+          message: 'Hi {previousValue}, nice to meet you!',
+          trigger: 'areyou',
+      },
+      {
+          id: 'areyou',
+          message: 'Why are you here?',
+          trigger: 'search',
+      },
+      {
+          id: 'search',
+          user: true,
+          trigger: '3',
+      },
+      {
+          id: '3',
+          component: <DBPedia />,
+          waitAction: true,
+          trigger: 'search',
+      },
+      {
+        id: 'end',
+        message: 'Thank you',
+        end: true,
+      },
+        // {
+        //   id: 'selectsomething',
+        //   message: 'Select some of the things',
+        //   trigger: '4',
+        // },
+        // {
+        //     id: '4',
+        //     options: [
+        //       { value: 1, label: 'Enjoy', trigger: '5' },
+        //       { value: 2, label: 'News', trigger: '5' },
+        //       { value: 3, label: 'Chatting', trigger: '5' },
+        //     ]
+        //   },
+        // {
+        //   id: '5',
+        //   component: <DBPedia />,
+        //   waitAction: true,
+        //   trigger: 'search',
+        // }
+        
+      ]
+
+      this.setState({
+        steps
+      })
+  }
+   
+
+  render() {
+    let steps =  this.state.steps
+    if(steps.length === 0 ){
+      return <Loading/>
+    }
+    return (
+      <ChatBot
+        opened={true}
+        steps={steps}
+      />
+    )
+  }
+  
+}
 export default ExampleDBPedia;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 
 
-export const promise = (value, message = "") => new Promise(function(resolve, reject) { 
+export const promise = (value, message = "", extra = 1) => new Promise(function(resolve, reject) { 
      console.log(value, message)
      if(!value){
       return reject("something went wrong")
@@ -8,8 +8,45 @@ export const promise = (value, message = "") => new Promise(function(resolve, re
      if(value === "get"){
       return resolve("vishal"); 
      }else if(value === "reply"){
+         let res = {
+          value : message,
+          next : {
+              id: 'areyou',
+              message: 'Why are you here?',
+              trigger: 'search',
+            }
+          }
+        if (extra === 2){
+          res.next = {
+            id: 'gender',
+            options: [
+              { value: 'male', label: 'Male', trigger: '3' },
+              { value: 'female', label: 'Female', trigger: '3' },
+            ],
+          }
+        }
+        if(extra === 3){
+          res.next =
+          {
+            id: 'age',
+            user: true,
+            trigger: '3',
+            placeholder :"Enter a number",
+            validator: (value) => {
+              if (isNaN(value)) {
+                return 'value must be a number';
+              } else if (value < 0) {
+                return 'value must be positive';
+              } else if (value > 120) {
+                return `${value}? Come on!`;
+              }
+
+              return true;
+            },
+          }
+        }
          setTimeout(()=>{
-           return resolve(message)
+           return resolve(res)
          },1000)
      }
 
